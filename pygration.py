cade36cb05dd7f88e2453a6b8abf657887d7820d
Matthew Graham <mdg149@optonline.net>
2009-01-17 18:39:30 -0500

    Pygration now includes functions for the 3 stages
    added commented prototypes for add, hide, drop, rollback_add, rollback_hide

--- pygration.py
+++ pygration.py
@@ -10,6 +10,26 @@ class Pygration:
     def __init__( self ):
         pass
 
+    def add( self, db ):
+        """Add elements to the db"""
+        pass
+
+    def hide( self, db ):
+        """Hide elements in the db before dropping them"""
+        pass
+
+    def drop( self, db ):
+        """Permanently drop elements from the db"""
+        pass
+
+    def rollback_add( self, db ):
+        """Rollback any items that were added to the db"""
+        pass
+
+    def rollback_hide( self, db ):
+        """Rollback any items that were hidden in the db"""
+        pass
+
     def pre_up( self, step ):
         pass
 
