65ce39169507e0655f479725a0c5b6f1a91679c4
Matthew Graham <mdg149@optonline.net>
2009-01-17 17:40:34 -0500

    added a PygrationStep class to pygrate.py

--- pygrate.py
+++ pygrate.py
@@ -2,6 +2,17 @@
 import pygration
 
 
+class PygrationStep:
+    """A single step for a migration.
+    """
+
+    def execute_sql( self, sql ):
+        self._sql = sql
+
+    def execute_sql_file( self, sql_file ):
+        self._sql_file = sql_file
+
+
 class Pygrator:
     """The operator for running a set of pygrations.
     """
