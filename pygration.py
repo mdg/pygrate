cfad1031fc186681ea2d4026cc5e02051e6a9b94
Matthew Graham <mdg149@optonline.net>
2009-01-17 14:00:07 -0500

    separated pygrate.py into 2 files
    pygrate.py is now the command line interface
    pygration.py is the classes for users to import and derive from

---
+++ pygration.py
@@ -0,0 +1,29 @@
+
+
+class Pygration:
+    """A single change to the database.
+    """
+
+    def __init__( self ):
+        pass
+
+    def pre_up( self ):
+        pass
+
+    def pre_down( self ):
+        pass
+
+    def post_up( self ):
+        pass
+
+    def post_down( self ):
+        pass
+
+
+class Pygrant(Pygration):
+    """A database grant that will be run for each release.
+    """
+
+    def __init__( self ):
+        pass
+
