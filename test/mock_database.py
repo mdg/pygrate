0a93ceb6a88f646158a24f29a5947388a182b394
Matthew Graham <mdg149@optonline.net>
2009-01-19 13:36:14 -0500

    made mock_database be a database.Connection
    removed all the Syntax methods

--- test/mock_database.py
+++ test/mock_database.py
@@ -1,9 +1,8 @@
 import database
 
 
-class MockDatabase(database.Database):
-    """An actual database
-    """
+class MockConnection(database.Connection):
+    """An mocked version of an actual DB connection."""
 
     def __init__( self ):
         self._last_sql = ""
@@ -13,28 +12,6 @@ class MockDatabase(database.Database):
 
     def last_sql( self ):
         return self._last_sql
-
-    def create_table_sql( self, table_name, columns ):
-        sql = "create table "+ table_name +" ("
-        for c in columns:
-            sql += c.type() +" "+ c.name() +", "
-        sql += ")"
-        return sql
-
-    def rename_table( self, old_table_name, new_table_name ):
-        pass
-
-    def drop_table( self, table_name ):
-        pass
-
-    def add_column( self, table, column_obj ):
-        pass
-
-    def rename_column( self, table, old_name, new_name ):
-        pass
-
-    def drop_column( self, table, column_name ):
-        pass
 
     def execute_sql( self, sql ):
         self._last_sql = sql
