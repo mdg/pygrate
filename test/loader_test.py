885f3976aea6d1338d691e73353b891e965101e4
Matthew Graham <mdg149@optonline.net>
2009-01-26 23:28:17 -0500

    fixed the underscore_is_pygration
    it was mismatched

--- test/loader_test.py
+++ test/loader_test.py
@@ -60,11 +60,11 @@ class VersionTestCase(unittest.TestCase):
 
     def test_underscore_is_pygration(self):
         """Check that v0_0_0 is reported as a pygration version."""
-        v = pygrate.loader.Version("v0_1_2")
+        v = pygrate.loader.Version("v1_2_13")
         self.assertTrue( v.is_pygration() )
         self.assertEqual(v._component(0), "1")
         self.assertEqual(v._component(1), "2")
-        self.assertEqual(v._component(2), "3")
+        self.assertEqual(v._component(2), "13")
 
     def test_dash_is_pygration(self):
         """Check that v0-0-0 is reported as a pygration version."""
