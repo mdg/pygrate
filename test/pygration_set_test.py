297e6f5c7b8262b942ef03276df20ba4e9856020
Matthew Graham <mdg149@optonline.net>
2009-05-05 22:49:30 -0400

    pygration_set test now uses drop & commit_drop
    hide, drop and rollback_hide were replaced with
    drop, commit_drop and rollback_drop.

--- test/pygration_set_test.py
+++ test/pygration_set_test.py
@@ -14,16 +14,16 @@ class PygrationTracker(Pygration):
         self._operation = 'add'
 
     def hide(self,db):
-        self._operation = 'hide'
+        self._operation = 'drop'
 
     def drop(self,db):
-        self._operation = 'drop'
+        self._operation = 'commit_drop'
 
     def rollback_add(self,db):
         self._operation = 'rollback_add'
 
     def rollback_hide(self,db):
-        self._operation = 'rollback_hide'
+        self._operation = 'rollback_drop'
 
 
 class PygrationSetTestCase(unittest.TestCase):
@@ -38,23 +38,23 @@ class PygrationSetTestCase(unittest.TestCase):
 
         self.assertTrue("add",self._pygration[0]._operation)
 
-    def testHide(self):
-        self._set.migrate(self._pygrator,"hide")
-
-        self.assertTrue("hide",self._pygration[0]._operation)
-
     def testDrop(self):
         self._set.migrate(self._pygrator,"drop")
 
         self.assertTrue("drop",self._pygration[0]._operation)
+
+    def testCommitDrop(self):
+        self._set.migrate(self._pygrator,"commit_drop")
+
+        self.assertTrue("commit_drop",self._pygration[0]._operation)
 
     def testRollbackAdd(self):
         self._set.migrate(self._pygrator,"rollback_add")
 
         self.assertTrue("rollback_add",self._pygration[0]._operation)
 
-    def testRollbackHide(self):
-        self._set.migrate(self._pygrator,"rollback_hide")
+    def testRollbackDrop(self):
+        self._set.migrate(self._pygrator,"rollback_drop")
 
-        self.assertTrue("rollback_hide",self._pygration[0]._operation)
+        self.assertTrue("rollback_drop",self._pygration[0]._operation)
 
