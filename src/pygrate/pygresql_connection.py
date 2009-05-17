d58f79f39c786dbfba6eb66a2a0569cf0af94619
Matthew Graham <mdg149@optonline.net>
2009-05-17 15:44:13 -0400

    better option handling in pygresql connection

--- src/pygrate/pygresql_connection.py
+++ src/pygrate/pygresql_connection.py
@@ -8,10 +8,22 @@ class PygresqlConnection(database.Connection):
 
     def __init__(self, opts):
         database.Connection.__init__(self, pg_syntax.PgSyntax())
-        db = opts['database']
-        user = opts['prospekz_dba']
-        passwd = opts['passwd']
-        self._conn = pg.connect(dbname=db, user=user, passwd=passwd)
+        # these values come from the given database.d config file
+        db = host = user = password = None
+        if 'database' in opts:
+            db = opts['database']
+        if 'host' in opts:
+            host = opts['host']
+        if 'user' in opts:
+            user = opts['user']
+        if 'password' in opts:
+            password = opts['password']
+
+        if True or 'verbose' in opts:
+            print "pg.connect(dbname=%s, host=%s, user=%s, passwd=%s)" \
+                    % (db, host, user, password)
+        self._conn = pg.connect(dbname=db, host=host, user=user
+                , passwd=password)
 
     def close( self ):
         """Close this pygresql connection."""
