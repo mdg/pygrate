e610e147bef5e238d684d674f1ad000d0e3b2cc2
Matthew Graham <mdg149@optonline.net>
2009-07-05 17:51:08 -0400

    few more steps toward history implementation
    history files are now run by bin/pygrate but don't yet do anything

--- pygration/history.py
+++ pygration/history.py
@@ -1,12 +1,23 @@
+import sqlalchemy
+from sqlalchemy import Column, Integer, String
 
 
 class History:
     """A class for loading version history from the DB.  It also writes
     history back to the DB as it changes."""
 
-    def __init__(self, schema, dbconn):
+    metadata = sqlalchemy.MetaData()
+    version_history_table = sqlalchemy.Table('version_history', metadata
+            , Column('version_number', String, primary_key=True)
+            , Column('sequence', Integer)
+            , Column('add', String)
+            , Column('drop', String)
+            , Column('commit', String)
+            )
+
+    def __init__(self, engine, schema=None):
+        self._engine = engine
         self._schema = schema
-        self._db = dbconn
 
     def load(self):
         pass
@@ -16,4 +27,9 @@ class History:
 
     def versions(self):
         return []
+
+def load(dbconn, schema=None):
+    h = History(dbconn, schema)
+    h.load()
+    return h
 
