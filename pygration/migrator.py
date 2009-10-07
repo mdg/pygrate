84dfc39203ab39a68b1e9dd0d64d82e628931ae2
Calen Pennington <cpennington@wgen.net>
2009-10-07 11:42:24 -0400

    Rewriting the arg parsing code to handle ff not taking a migration

--- pygration/migrator.py
+++ pygration/migrator.py
@@ -284,6 +284,10 @@ class Migrator(object):
             print columns % (m.step_id(), m.step_name(), add_state
                     , simdrop_state, drop_state)
 
+    def fastforward(self):
+        for m in self._migration_set:
+            print m
+
     def has_version(self, migration):
         for s in self._steps:
             if s.version() == migration:
